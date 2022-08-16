a4c9f58898805186ef12eab1568328e5c4ad9503
Mehdi Naoufel <mehdinaoufel@gmail.com>
2022-08-16 14:18:15 -0400

    implemented active record and cloudinary bundles

--- app/models/ghost.rb
+++ app/models/ghost.rb
@@ -1,6 +1,7 @@
 class Ghost < ApplicationRecord
   belongs_to :user
   has_many :reviews, through: :spooks
+  has_one_attached :photo
 
   validates :name, :spook_action, :location, :description, :daily_rate, presence: true
 end
