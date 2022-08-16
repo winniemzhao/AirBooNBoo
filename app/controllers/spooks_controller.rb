707acaf448f098e6686cb0404c3747b556fb350c
sarah fulham <sarahlfulham@gmail.com>
2022-08-16 16:43:33 -0400

    created new and create action with form view and show view

--- app/controllers/spooks_controller.rb
+++ app/controllers/spooks_controller.rb
@@ -1,4 +1,7 @@
 class SpooksController < ApplicationController
+  before_action :set_spook, only: :destroy
+  before_action :set_ghost, only: [:new, :create]
+
   def my_spooks
     # As a user, I can see all the ghosts that I've rented
     @spooks = Spook.all
@@ -11,9 +14,18 @@ class SpooksController < ApplicationController
 
   def new
     # As a user, I can input details about my spook (start date, end date)
+    @spook = Spook.new
   end
 
   def create
+    @spook = Spook.new(spook_params)
+    @spook.ghost = @ghost
+    @spook.user = current_user
+    if @spook.save
+      redirect_to my_spook_path(@spook)
+    else
+      render :new, status: 422
+    end
   end
 
   def edit
@@ -25,5 +37,15 @@ class SpooksController < ApplicationController
 
   def destroy
     # As a user, I can cancel a spook/haunting/request
+  end
+
+  private
+
+  def spook_params
+    params.require(:spook).permit(:start_date, :end_date)
+  end
+
+  def set_ghost
+    @ghost = Ghost.find(params[:ghost_id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,28 @@ require 'faker'
       password: "password"
   )
 end
+
+10.times do
+  ghost = Ghost.new(
+    name: Faker::Movies::Ghostbusters.character,
+    spook_action: %w[interactive ectoplasm poltergeist orb funnel].sample,
+    location: %w[Manchester Paris Toronto],
+    description: Faker::Lorem.paragraph,
+    daily_rate: Faker::Number.decimal(l_digits: 2)
+  )
+  ghost.user = [User.first, User.last].sample
+  ghost.save!
+end
+
+puts "Creating spooky spooks..."
+
+10.times do
+  spook = Spook.new(
+    start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+    end_date: Faker::Date.between(from: '2014-10-01', to: '2014-10-03'),
+    status: rand(0..2)
+  )
+  spook.ghost = [Ghost.first, Ghost.last].sample
+  spook.user = spook.ghost.user == User.first ? User.last : User.first
+  spook.save!
+end
