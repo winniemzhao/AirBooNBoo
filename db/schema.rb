c55597fe658b34e71e71f93d625a545d3df467c1
sarah fulham <sarahlfulham@gmail.com>
2022-08-17 11:50:16 -0400

    gem file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_181040) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_153121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,7 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_181040) do
     t.date "start_date"
     t.date "end_date"
     t.float "total_price"
-    t.integer "status"
+    t.integer "status", default: 0
     t.bigint "ghost_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
