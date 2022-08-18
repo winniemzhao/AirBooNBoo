03d2c52e77cf5e0266b84a3a6bba14ff24b13f39
Michael Wales <wales@aya.yale.edu>
2022-08-18 14:44:16 -0700

    Log out button classed a dropdown-item

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_193356) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "ghosts", force: :cascade do |t|
     t.string "name"
     t.string "spook_action"
